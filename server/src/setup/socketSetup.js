ddfeebb6b341b9f64f53f29d62d0f66f5453a768
Danil Korsun <dakorsun@gmail.com>
2019-11-18 22:14:32 +0200

    Socket: small fix

--- server/src/setup/socketSetup.js
+++ server/src/setup/socketSetup.js
@@ -1,13 +1,12 @@
 const socketIO = require('socket.io');
 console.log('a racer connected');
-import {SOCKET_RACE_STEP} from '../../../shared/constants/socketActions'
 
 const socketSetup = server => {
     const io = socketIO(server);
     io.on('connection', function (socket) {
         console.log('socket: a client connected');
 
-        socket.emit(SOCKET_RACE_STEP);
+        socket.emit('SOCKET_RACE_STEP');
     });
 };
 
