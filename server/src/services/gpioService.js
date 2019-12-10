59d586ff29f04500b8e745e341e173a8ec9e6bf0
Danil Korsun <dakorsun@gmail.com>
2019-12-10 17:20:03 +0200

    GPIO: initial setup

--- server/src/services/gpioService.js
+++ server/src/services/gpioService.js
@@ -1,4 +1,15 @@
 import gpio from 'rpi-gpio';
+import serverConfig from '../../config/serverConfig'
+const {RACER_ONE, RACER_TWO} = serverConfig;
+
+
+gpio.on('change', function(channel, value) {
+    console.log('Channel ' + channel + ' value is now ' + value);
+});
+
+gpio.setup(RACER_ONE.HALL_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+gpio.setup(RACER_TWO.HALL_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 const gpioService = {
 
