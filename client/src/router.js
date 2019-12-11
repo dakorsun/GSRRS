eb6fc16a1bd48fbd7378564619b45269899796a6
Danil Korsun <dakorsun@gmail.com>
2019-12-11 19:30:50 +0200

    merge fix

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Test from './views/Test'
 
 Vue.use(Router);
 
--- server/src/setup/socketSetup.js
+++ server/src/setup/socketSetup.js
@@ -1,5 +1,4 @@
 import socketIO from 'socket.io';
-import {SOCKET_RUN_FINISHED, SOCKET_RUN_RESULT, SOCKET_START_RUN} from '../../../shared/constants/socketActions'
 
 export default server => {
     const io = socketIO(server);
