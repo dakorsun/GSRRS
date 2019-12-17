774ab7398f2ad6561eac861114c6cdd388057bcc
Danil Korsun <dakorsun@gmail.com>
2019-12-17 20:16:16 +0200

    fix

--- server/src/services/gpioService.js
+++ server/src/services/gpioService.js
@@ -1,10 +1,9 @@
-import gpio, {promise as gpiop} from 'rpi-gpio';
+import gpio from 'rpi-gpio';
 
 import serverConfig from '../../config/serverConfig'
 
 const {BIKE_ONE, BIKE_TWO} = serverConfig;
 
-const {RACER_ONE, RACER_TWO} = serverConfig;
 try{
 gpio.on('connection', () => {
 	console.log('gpio interface connected');
@@ -14,69 +13,30 @@ gpio.on('change', function(channel, value) {
     console.log('Channel ' + channel + ' value is now ' + value);
 });
 
-gpiop.on('change')
-    .then(function (channel, value) {
-        console.log('Promise Channel ' + channel + ' value is now ' + value);
-    });
 gpio.on('error', (err)=> {
 	console.error(err);
 })
 
-const used = [1,2,4,6,9,14,17,20,25,30,39];
-//try{
+const buttons = require('rpi-gpio-buttons')([BIKE_ONE.HALL_PIN]);
 
-//for(let i = 1; i++; i <= 40){
-//	if(!(used.findIndex(x => x === i) + 1)){
-//		gpio.setup(i, gpio.DIR_IN, gpio.EDGE_BOTH);
-//	}
-//}
-//}catch(e){
-//	console.error(e);
-//}
-
-const buttons = require('rpi-gpio-buttons')([RACER_ONE.HALL_PIN]);
-
-buttons.on('pressed', function (pin) {
-  console.log('User pressed button on pin ', pin);
-});
-
-gpiop.setup(BIKE_ONE.HALL_PIN, gpiop.DIR_IN, gpiop.EDGE_BOTH);
-
-gpiop.setup(BIKE_TWO.HALL_PIN, gpiop.DIR_IN, gpiop.EDGE_BOTH);
-buttons.on('clicked', function (pin) {
-  console.log('User clicked button on pin ', pin);
-});
-
-buttons.on('clicked_pressed', function (pin) {
-  console.log('User clicked then pressed button on pin ', pin);
-});
+// buttons.on('pressed', function (pin) {
+//   console.log('User pressed button on pin ', pin);
+// });
+//
+// buttons.on('clicked', function (pin) {
+//   console.log('User clicked button on pin ', pin);
+// });
+//
+// buttons.on('clicked_pressed', function (pin) {
+//   console.log('User clicked then pressed button on pin ', pin);
+// });
 
 buttons.on('button_changed', function (pin) {
 	  console.log('Changed button on pin ', pin);
 });
 
-	//gpio.setup(RACER_ONE.HALL_PIN, gpio.DIR_IN, gpio.EDGE_BOTH);
-
-	//setInterval(() => {
-	//	gpio.read(RACER_ONE.HALL_PIN, (state, ...args) => {
-	//		console.log(RACER_ONE.HALL_PIN + ' pin state: ', state, ...args)
-	//	})
-	//}, 2000)
-
-//gpio.setup(RACER_ONE.HALL_PIN, gpio.DIR_IN, gpio.EDGE_NONE);
-
-//gpiop.on('change')
-  //  .then(function (channel, value) {
-    //    console.log('Channel ' + channel + ' value is now ' + value);
-    //});
-
-//gpiop.setup(RACER_ONE.HALL_PIN, gpiop.DIR_IN, gpiop.EDGE_BOTH);
-
-
-
-//gpiop.setup(RACER_TWO.HALL_PIN, gpiop.DIR_IN, gpiop.EDGE_BOTH);
-
 console.log('gpio setted up');
+
 }catch(e){
 	console.error(e);
 }
