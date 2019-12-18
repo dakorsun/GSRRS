57998a7d258f96971266b8eb74d1a504126ecec5
Danil Korsun <dakorsun@gmail.com>
2019-12-18 21:23:42 +0200

    testing

--- server/src/services/gpioService.js
+++ server/src/services/gpioService.js
@@ -4,60 +4,62 @@ import serverConfig from '../../config/serverConfig'
 
 const {BIKE_ONE, BIKE_TWO} = serverConfig;
 
-try{
-gpio.on('connection', () => {
-	console.log('gpio interface connected');
-})
+try {
+    gpio.on('connection', () => {
+        console.log('gpio interface connected');
+    })
 
-// gpio.on('change', function(channel, value) {
-//     console.log('Channel ' + channel + ' value is now ' + value);
-// });
 
-gpio.on('error', (err)=> {
-	console.error(err);
-})
+    gpio.on('error', (err) => {
+        console.error(err);
+    })
 
-const buttons = require('rpi-gpio-buttons')([BIKE_ONE.HALL_PIN]);
+    const buttons = require('rpi-gpio-buttons')([BIKE_ONE.HALL_PIN]);
 
-buttons.on('pressed', function (pin) {
-  console.log('User pressed button on pin ', pin);
-});
+    buttons.on('pressed', function (pin) {
+        console.log('User pressed button on pin ', pin);
+    });
 
-buttons.on('released', function (pin) {
-  console.log('User released button on pin ', pin);
-});
+    buttons.on('released', function (pin) {
+        console.log('User released button on pin ', pin);
+    });
 
-buttons.on('clicked', function (pin) {
-  console.log('User clicked button on pin ', pin);
-});
+    buttons.on('clicked', function (pin) {
+        console.log('User clicked button on pin ', pin);
+    });
 
-buttons.on('button_click', function (pin) {
-  console.log('User button_clicked button on pin ', pin);
-});
+    buttons.on('button_click', function (pin) {
+        console.log('User button_clicked button on pin ', pin);
+    });
 
-buttons.on('button_release', function (pin) {
-  console.log('User button_release button on pin ', pin);
-});
+    buttons.on('button_release', function (pin) {
+        console.log('User button_release button on pin ', pin);
+    });
 
-buttons.on('clicked_pressed', function (pin) {
-  console.log('User clicked_pressed then pressed button on pin ', pin);
-});
+    buttons.on('clicked_pressed', function (pin) {
+        console.log('User clicked_pressed then pressed button on pin ', pin);
+    });
 
-buttons.on('button_changed', function (pin) {
-	  console.log('Changed button on pin ', pin);
-});
+    buttons.on('button_changed', function (pin) {
+        console.log('Changed button on pin ', pin);
+    });
 
-gpio.setup(BIKE_TWO.HALL_PIN, gpio.DIR_LOW, gpio.EDGE_BOTH)
+    gpio.setup(BIKE_TWO.HALL_PIN, gpio.DIR_LOW, gpio.EDGE_BOTH)
 
-console.log('gpio setted up');
+    gpio.on('change', function (channel, value) {
+        console.log('Channel ' + channel + ' value is now ' + value);
+    });
 
-}catch(e){
-	console.error(e);
+
+    console.log('gpio setted up');
+
+} catch (e) {
+    console.error('gpio setup error: ', e);
 }
 
 
 const GPIOService = {
-	gpio
+    gpio
 };
 
 export default GPIOService;
